0ddf3057b785103354a7c8987aad5d2ddfdc371f
Mathew Chebet <kiproye@gmail.com>
2024-12-27 20:33:58 -0500

    Completed: - A simple home page for the frontend of the application (need to improve reponsiveness) -A user dto that will be used when a user subscribes Dec. 27, 2024

---
+++ LanguageProjectFrontend/DtoModels/UsersCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanguageProjectFrontend.DtoModels
+{
+    //This class will be used to model an external representation of a user upon creation.
+    public class UsersCreateDto
+    {
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        public string LanguagePreference { get; set; } //The language they want to learn.
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z0-9]+@[A-Za-z0-9]+/.+[^@/s]")]
+        public string Email { get; set; } //must be unique
+
+        [Required]
+        public string EmailFrequency { get; set; }
+
+    }
+}
