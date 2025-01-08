b68140ebb72c5ca16b3a0f1c879a902d45356008
Mathew Chebet <kiproye@gmail.com>
2025-01-08 16:33:09 -0500

    Starting to build the azure function to automate emails. Jan 8, 2025

---
+++ EmailAzureFunction/EmailScheduler.cs
@@ -0,0 +1,46 @@
+using LanguageProjectBackend.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace EmailAzureFunction
+{
+    public class EmailScheduler
+    {
+        private readonly ILogger _logger;
+        private readonly EmailSender _sender;
+
+        public EmailScheduler(ILoggerFactory loggerFactory, EmailSender emailSender)
+        {
+            _logger = loggerFactory.CreateLogger<EmailScheduler>();
+            _sender = emailSender;
+        }
+
+        [Function("SendEmail")]
+        public void Run([TimerTrigger("* 15 10 * * * ")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+
+            int currentDate = DateTime.UtcNow.Day;
+            DayOfWeek currentDay = DateTime.UtcNow.DayOfWeek;
+
+            _sender.SendNewWordEmail("Daily");
+
+            //Send email to weekly subscribers
+            if (currentDay == DayOfWeek.Monday)
+            {
+                _sender.SendNewWordEmail("Monthly");
+            }
+
+            //Send email to monthly subscribers
+            if (currentDate == 1)
+            {
+                _sender.SendNewWordEmail("Monthly");
+            }
+        }
+    }
+}
---
+++ EmailAzureFunction/Program.cs
@@ -0,0 +1,26 @@
+using LanguageProjectBackend.Data;
+using LanguageProjectBackend.Services;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddDbContext<LanguageProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+builder.Services.AddScoped<IUserRepo, UserRepository>();
+builder.Services.AddScoped<IWordRepo, WordRepository>();
+builder.Services.AddScoped<IUserWordRepo, UserWordRepository>();
+builder.Services.AddScoped<EmailSender>();
+builder.Services.AddScoped<Translator>();
+
+builder.Build().Run();
