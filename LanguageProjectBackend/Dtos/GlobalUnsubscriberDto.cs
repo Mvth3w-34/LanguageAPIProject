2cbea13465492869868d3ec37abffd2a0eccfe0b
Mathew Chebet <kiproye@gmail.com>
2025-01-09 17:51:35 -0500

    Working on the ability to delete a user. Jan 9, 2025.

---
+++ LanguageProjectBackend/Dtos/GlobalUnsubscriberDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LanguageProjectBackend.Dtos
+{
+    public class GlobalUnsubscriberDto
+    {
+        public string Email { get; set; }
+        public string Reason { get; set; }
+        public string Status { get; set; }
+        public long CreatedAt { get; set; }
+        public long UpdatedAt { get; set; }
+    }
+}
